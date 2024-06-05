499429a042806dd33df2caabe2d79facc826028f
Yesset04 <yesset.yedres@nu.edu.kz>
2024-06-05 19:44:27 +0500

    Contact page texts create

--- src/components/welcome/contactsUs/contactsUs.tsx
+++ src/components/welcome/contactsUs/contactsUs.tsx
@@ -1,33 +1,47 @@
-import WelcomeFooter from '../layout/welcomeFooter/welcomeFooter';
-import WelcomeHeader from '../layout/welcomeHeader/welcomeHeader';
-import TextField from '@mui/material/TextField';
-import './contactUs.scss'
-import { Button } from '@mui/material';
+import WelcomeFooter from "../layout/welcomeFooter/welcomeFooter";
+import WelcomeHeader from "../layout/welcomeHeader/welcomeHeader";
+import TextField from "@mui/material/TextField";
+import "./contactUs.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
 
 const ContactUs = () => {
-    return (
-        <div className="contact">
-            <WelcomeHeader/>
-            <div className="contact-content">
-                <div className="container">
-                    <div className="contact-content-inside">
-                        <div className="contact-texts">
-                            madlak
-                        </div>
-                    <div className="contact-form">
-                        <form className='form'>
-                            <TextField className='inputs' id="outlined-basic" label="Name..." variant="outlined" />
-                            <TextField className='inputs' id="outlined-basic" label="Email..." variant="outlined" />
-                            <TextField className='message' id="outlined-basic" label="Your message..." variant="outlined" />
-                            <Button className='send-button' type='submit' variant='contained'>Send Message</Button>
-                        </form>
-                    </div>
-                </div>
-                </div>
+  return (
+    <div className="contact">
+      <WelcomeHeader />
+      <div className="contact-content">
+        <div className="container">
+          <div className="contact-content-inside">
+            <div className="contact-texts">
+              <p className="contact-texts-default">Contact Us</p>
+              <p className="contact-texts-header">
+                We'd love to hear from you!
+              </p>
+              <p className="contact-texts-default">
+                Share your feedback and ask your questions.
+              </p>
+              <p className="contact-texts-credentials">
+                <FontAwesomeIcon icon={faEnvelope} /> wonkaikz@gmail.com
+              </p>
+              <p className="contact-texts-credentials">
+                <FontAwesomeIcon icon={faPhone} /> +7 (706) 428-59-23
+              </p>
             </div>
-            <WelcomeFooter/>
+            <div className="contact-form">
+              <form className="form">
+                <TextField
+                  id="outlined-basic"
+                  label="Outlined"
+                  variant="outlined"
+                />
+              </form>
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+      <WelcomeFooter />
+    </div>
+  );
+};
 
 export default ContactUs;
