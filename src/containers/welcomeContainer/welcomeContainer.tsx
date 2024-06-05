aee63db42f2af4f3587ac6a129c12f7b2d7c154e
nurkeldi <amirovmarat1969@gmail.com>
2024-06-05 14:24:08 +0500

    Created components

---
+++ src/containers/welcomeContainer/welcomeContainer.tsx
@@ -0,0 +1,11 @@
+import './welcomeContainer.scss'
+
+const WelcomeContainer = () => {
+    return (
+        <div className="welcome-container">
+            <h1>Welcome</h1>
+        </div>
+    )
+}
+
+export default WelcomeContainer;
