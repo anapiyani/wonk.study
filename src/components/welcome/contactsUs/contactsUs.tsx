e65a50a2bfe55b30ee8bfd4cb689bb143e180375
nurkeldi <amirovmarat1969@gmail.com>
2024-06-05 18:59:03 +0500

    Creating contact page

---
+++ src/components/welcome/contactsUs/contactsUs.tsx
@@ -0,0 +1,20 @@
+import './contactUs.scss'
+
+const ContactUs = () => {
+    return (
+        <div className="contact">
+            <div className="container">
+                <div className="contact-texts">
+
+                </div>
+            </div>
+            <div className="contact-form">
+                <form>
+                    
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default ContactUs;
