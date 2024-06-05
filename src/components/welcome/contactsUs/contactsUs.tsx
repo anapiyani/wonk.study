3d5eeb571ec59c4195e82b9789df89b48fc57f51
nurkeldi <amirovmarat1969@gmail.com>
2024-06-05 19:13:37 +0500

    Autializate Autrakhten

--- src/components/welcome/contactsUs/contactsUs.tsx
+++ src/components/welcome/contactsUs/contactsUs.tsx
@@ -7,18 +7,18 @@ const ContactUs = () => {
     return (
         <div className="contact">
             <WelcomeHeader/>
-            <div className="container">
-                <div className="contact-content">
-                    <div className="container">
+            <div className="contact-content">
+                <div className="container">
+                    <div className="contact-content-inside">
                         <div className="contact-texts">
-
+                            madlak
                         </div>
-                    </div>
                     <div className="contact-form">
                         <form className='form'>
                             <TextField id="outlined-basic" label="Outlined" variant="outlined" />
                         </form>
                     </div>
+                </div>
                 </div>
             </div>
             <WelcomeFooter/>
