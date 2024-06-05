69ec70809575ab20b3b20b79970312beec5ade09
nurkeldi <amirovmarat1969@gmail.com>
2024-06-05 17:58:39 +0500

    Created header for welcome page

---
+++ src/components/welcome/aboutUs/aboutUs.tsx
@@ -0,0 +1,12 @@
+import WelcomeHeader from '../layout/welcomeHeader/welcomeHeader';
+import './aboutUs.scss';
+
+const AboutUs = () => {
+    return (
+        <div className="about-us">
+            <WelcomeHeader/>
+        </div>
+    )
+}
+
+export default AboutUs;
---
+++ src/components/welcome/layout/welcomeHeader/welcomeHeader.tsx
@@ -0,0 +1,22 @@
+import { NavLink } from 'react-router-dom';
+import logotype from '../../../../assets/logo_word.svg'
+import './welcomeHeader.scss';
+
+const WelcomeHeader = () => {
+    return (
+        <header>
+            <div className="container">
+                <div className="header-content">
+                    <NavLink to="/" className="logotype">
+                        <img src={logotype} alt="logotype" />
+                    </NavLink>
+                    <div className="choose-language">
+                        <p className="en active">Eng</p>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default WelcomeHeader;
