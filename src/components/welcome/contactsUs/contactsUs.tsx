1926f8afa9ccea17c0d99da1cb613009e0275fce
nurkeldi <amirovmarat1969@gmail.com>
2024-06-06 15:45:33 +0500

    Created email send, now all 200

--- src/components/welcome/contactsUs/contactsUs.tsx
+++ src/components/welcome/contactsUs/contactsUs.tsx
@@ -25,7 +25,6 @@ const ContactUs = () => {
         sender_email: email,
         message: message,
       }
-      console.log("Sending data:", dataSend); 
       dispatch(sendMessageEmail(dataSend))
     }
 
