adef8dd9ccab960e48b3dcf5d40ade86df392fcc
nurkeldi <amirovmarat1969@gmail.com>
2024-06-10 15:25:57 +0500

    Got info about user

--- src/config/axiosWonk.ts
+++ src/config/axiosWonk.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 axios.defaults.baseURL = "https://wonk-0d0b7ce7369a.herokuapp.com/";
 
-const axiosLogin = axios;
+const axiosWonk = axios;
 
-export default axiosLogin;
+export default axiosWonk;
