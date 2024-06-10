f454d9915079a30fcc2a6002e92e9383ee065834
nurkeldi <amirovmarat1969@gmail.com>
2024-06-10 16:08:04 +0500

    Getting user info and now i got them and tryinna show them on page

--- src/containers/teacherContainer/dashboardContainer/dashboadContainer.tsx
+++ src/containers/teacherContainer/dashboardContainer/dashboadContainer.tsx
@@ -1,6 +1,12 @@
+import { useSelector } from "react-redux";
 import TeacherDashboard from "../../../components/teacher/dashboard/teacherDashboard";
+import { RootState } from "../../../store/store";
 
 const DashboardContainer = () => {
+  const user = useSelector((state: RootState) => state.info.user);
+  const loading = useSelector((state: RootState) => state.info.isLoading);
+  const iserror = useSelector((state: RootState) => state.info.isError);
+
   return <TeacherDashboard />;
 };
 
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import emailReducer from "./email.slice";
 import loginReducer from "./login.slice";
+import infoReducer from "./info.slice";
 
 const store = configureStore({
   reducer: {
     email: emailReducer,
     login: loginReducer,
+    info: infoReducer,
   },
 });
 
