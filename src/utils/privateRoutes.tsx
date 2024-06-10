9986ba065203e990a11d83ffcef7ae7dadb2febf
nurkeldi <amirovmarat1969@gmail.com>
2024-06-10 14:53:29 +0500

    Added private routes/if logged in it'll be in teacher-dashboard if not then it'll be in welcome page

--- src/utils/privateRoutes.tsx
+++ src/utils/privateRoutes.tsx
@@ -2,7 +2,7 @@ import { Outlet, Navigate } from "react-router-dom";
 
 const PrivateRoutes = () => {
   const token = localStorage.getItem("accessToken");
-  return token ? <Outlet /> : <Navigate to="/login" />;
+  return token ? <Outlet /> : <Navigate to="/welcomePage" />;
 };
 
 export default PrivateRoutes;
