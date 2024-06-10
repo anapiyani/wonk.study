cdb8bf4b531e55c9870441a193f5a3f53fc5f76a
Yesset04 <yesset.yedres@nu.edu.kz>
2024-06-10 13:40:22 +0500

    teacherHeader update

---
+++ src/components/teacher/layout/teacherHeader/teacherHeader.tsx
@@ -0,0 +1,38 @@
+import { NavLink } from "react-router-dom";
+import logotype from "../../../../assets/logo_word.svg";
+import "./teacherHeader.scss";
+
+const WelcomeHeader = () => {
+  return (
+    <header className="teacher-header">
+      <div className="container">
+        <div className="teacher-header-content">
+          <div className="teacher-header-content-left">
+            <NavLink to="/" className="logotype">
+              <img src={logotype} alt="logotype" />
+            </NavLink>
+            <div className="choose-language">
+              <p className="en active">Eng</p>
+            </div>
+          </div>
+          <div className="teacher-header-content-right">
+            <div className="teacher-nav">
+              <NavLink to="/teacherDashboard" className="teacher-nav-link">
+                <p>Dashboard</p>
+              </NavLink>
+              <NavLink to="/teacherCourses" className="teacher-nav-link">
+                <p>Courses</p>
+              </NavLink>
+              <NavLink to="/teacherSettings" className="teacher-nav-link">
+                <p>Settings</p>
+              </NavLink>
+            </div>
+            <div className="logout-btn"></div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default WelcomeHeader;
