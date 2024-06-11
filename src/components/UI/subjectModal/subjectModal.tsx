bd8ec64cdab299d221e2edb4f4109ec8a3d99fa3
nurkeldi <amirovmarat1969@gmail.com>
2024-06-11 16:45:20 +0500

    Added component modal for one subject

--- src/components/UI/subjectModal/subjectModal.tsx
+++ src/components/UI/subjectModal/subjectModal.tsx
@@ -1,7 +1,24 @@
 import "./subjectModal.scss";
 
 const SubjectModal = () => {
-  return <div className="subject-modal"></div>;
+  return (
+    <div className="subject-modal">
+      <div className="backdrop">
+        <div className="subject-modal-content">
+          <div className="grade-select">
+            <div className="select-text">
+              <p>Select the grade</p>
+            </div>
+            <div className="grade">
+              <div className="grade-bg">
+                <div className="image"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SubjectModal;
