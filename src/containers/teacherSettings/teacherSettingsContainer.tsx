554156ed7788c513c257267e69bb8f04aa56c83a
nurkeldi <amirovmarat1969@gmail.com>
2024-06-11 18:45:45 +0500

    Addded logic for settings

--- src/containers/teacherSettings/teacherSettingsContainer.tsx
+++ src/containers/teacherSettings/teacherSettingsContainer.tsx
@@ -1,15 +1,68 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Settings from "../../components/teacher/settings/settings";
-import { RootState } from "../../store/store";
+import { AppDispatch, RootState } from "../../store/store";
+import { useEffect } from "react";
+import {
+  changeEmail,
+  changePassword,
+  removieSuccess,
+} from "../../store/info.slice";
+import { TtoChangePassword } from "../../types/types";
 
 const SettingsContainer = () => {
   const user = useSelector((state: RootState) => state.info.user);
   const courses = useSelector((state: RootState) => state.info.courses);
+  const dispatch = useDispatch<AppDispatch>();
+  const changeSuccess = useSelector(
+    (state: RootState) => state.info.successChange
+  );
+  const changeLoading = useSelector(
+    (state: RootState) => state.info.loadingChange
+  );
+  const changeErr = useSelector((state: RootState) => state.info.errorOnChange);
+
+  const updateEmail = (
+    e: React.FormEvent<HTMLFormElement>,
+    newEmail: string
+  ) => {
+    e.preventDefault();
+    const toUpdateEmail = {
+      email: newEmail,
+    };
+
+    dispatch(changeEmail(toUpdateEmail));
+  };
+  const updatePassword = (
+    e: React.FormEvent<HTMLFormElement>,
+    oldPassword: string,
+    newPassword: string
+  ) => {
+    e.preventDefault();
+    const toUpdatePassword: TtoChangePassword = {
+      old_password: oldPassword,
+      password: newPassword,
+    };
+
+    dispatch(changePassword(toUpdatePassword));
+  };
+
+  useEffect(() => {
+    if (changeSuccess) {
+      setInterval(() => {
+        dispatch(removieSuccess());
+      }, 2000);
+    }
+  }, [dispatch]);
 
   return (
     <Settings
       user={user}
       courses={courses}
+      changeSuccess={changeSuccess}
+      changeLoading={changeLoading}
+      changeErr={changeErr}
+      updateEmail={updateEmail}
+      updatePassword={updatePassword}
     />
   );
 };
