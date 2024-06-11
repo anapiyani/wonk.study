b8b4bc49c05b6b20c9063b1076ec549bda1d530a
Yesset04 <yesset.yedres@nu.edu.kz>
2024-06-11 18:09:00 +0500

    settingsPage styles completed

---
+++ src/components/teacher/dashboard/teacherMain/classCard.tsx
@@ -0,0 +1,30 @@
+// import "./classCard.css";
+
+// type TProps = {
+//   id: 0;
+//   course: {
+//     id: 0;
+//     name: string;
+//     course_img: string | null;
+//   };
+//   grade_level: string;
+//   section: string;
+// };
+
+// const ClassCard = (props: TProps) => {
+//   return (
+//     <div className="class-card">
+//       <div className="class-card-content">
+//         <div className="class-card-body">
+//           <h1 className="class-grade">{props.grade_level}{props.section}</h1>
+//           <h2>GRADE</h2>
+//         </div>
+//       </div>
+//       <div className="class-card-text">
+//         <h3>Show students(20)</h3>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ClassCard;
