76aa43ca801e57eb4ba7fd5ff5ed9ef5178ad711
nurkeldi <amirovmarat1969@gmail.com>
2024-06-11 15:16:42 +0500

    Fixin bugs

---
+++ src/components/UI/subjectsModal/subjectsModal.tsx
@@ -0,0 +1,27 @@
+import { TCoureses } from "../../../types/types";
+import SubjectCard from "../../teacher/dashboard/teacherMain/subjectCard";
+import "./subjectsModal.scss";
+
+type TProps = {
+  courses: TCoureses[];
+};
+
+const SubjectsModal = (props: TProps) => {
+  return (
+    <div className="subjects-modal">
+      <div className="backdrop">
+        <div className="subject-modal-content">
+          {props.courses.map((item) => (
+            <SubjectCard
+              id={item.id}
+              course_img={item.course_img}
+              name={item.name}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SubjectsModal;
