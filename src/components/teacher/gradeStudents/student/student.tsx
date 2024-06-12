74470a9a655010529df351fc5f44b894390ca187
nurkeldi <amirovmarat1969@gmail.com>
2024-06-12 19:35:59 +0500

    Added click user

--- src/components/teacher/gradeStudents/student/student.tsx
+++ src/components/teacher/gradeStudents/student/student.tsx
@@ -3,16 +3,20 @@ import { TStudent } from "../../../../types/types";
 
 type StudentProps = {
   student: TStudent;
+  onClickStudent: (id: number) => void;
 };
 
-const Student = ({ student }: StudentProps) => {
+const Student = (props: StudentProps) => {
   return (
-    <div className="student-component">
+    <div
+      onClick={() => props.onClickStudent(props.student.id)}
+      className="student-component"
+    >
       <div className="student-info-all">
         <div className="student-picture">
           <img
             src={
-              student.profile_picture ||
+              props.student.profile_picture ||
               "https://cdn.pixabay.com/photo/2021/06/07/13/46/user-6318008_640.png"
             }
             alt="icon-user"
@@ -21,9 +25,9 @@ const Student = ({ student }: StudentProps) => {
         <div className="student-info">
           <div className="student-name">
             <p className="student-name-inside">
-              {student.last_name} {student.first_name}
+              {props.student.last_name} {props.student.first_name}
             </p>
-            <p className="student-id">Student ID: {student.id}</p>
+            <p className="student-id">Student ID: {props.student.id}</p>
           </div>
         </div>
       </div>
