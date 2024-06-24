f78235f4feaa7679cae8a5dcd7c7528346735175
Abyl10 <abylay.kairatbek@gmail.com>
2024-06-24 14:21:50 +0500

    fix: imports

---
+++ src/components/welcome/index.ts
@@ -0,0 +1,7 @@
+import AboutUs from "./aboutUs/aboutUs";
+import ContactUs from "./contactsUs/contactsUs";
+import PrivacyPolicy from "./policy/privacyPolicy";
+import TermsOfUse from "./termsOfUse/termsOfUse";
+import WelcomePage from "./welcomePage/welcomePage";
+
+export { AboutUs, ContactUs, PrivacyPolicy, TermsOfUse, WelcomePage };
